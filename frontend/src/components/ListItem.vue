dee07c515c2dcae11357df979169d76c2563b95d
Christian Valdez <christianvaldez36@hotmail.com>
2021-04-04 22:49:51 -0700

    created component for list items

---
+++ frontend/src/components/ListItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <li
+    class="group flex justify-between items-center border border-gray-300 rounded-lg shadow-md px-4 py-3"
+  >
+    <div class="truncate">
+      <a
+        class="text-xl text-purple-600 truncate overflow-hidden"
+        :href="url"
+        target="_blank"
+      >
+        <p class="truncate">
+          {{ url }}
+        </p>
+      </a>
+      <p class="overflow-y-auto">{{ title }}</p>
+    </div>
+    <span
+      @click="deleteRecord"
+      class="text-gray-100 group-hover:text-gray-700 cursor-pointer"
+    >
+      &#10005;
+    </span>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    deleteRecord() {
+      this.$emit('deleteRecord');
+    },
+  },
+};
+</script>
