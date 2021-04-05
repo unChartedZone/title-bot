78550cdda9fea5283a3d2a579abeed8c8965ed99
Christian Valdez <christianvaldez36@hotmail.com>
2021-04-04 22:50:53 -0700

    main page now allows adding records of url titles, and removing records

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,42 +1,119 @@
 <template>
-  <div id="app">
-    <h1>Title Bot</h1>
-    <form @submit.prevent="storeRecord">
-      <input v-model="urlInput" type="text" placeholder="URL" />
-      <button type="submit">Submit</button>
-    </form>
-    <ul>
-      <li v-for="record in records" :key="record._id">
-        {{ record.title }}
-      </li>
-    </ul>
-  </div>
+  <main id="app">
+    <div class="container mx-auto">
+      <h1 class="text-5xl text-center text-purple-600 my-8 header">
+        Title Bot
+      </h1>
+      <form
+        class="sm:w-2/5 space-y-4 mx-4 sm:mx-auto my-12"
+        @submit.prevent="storeRecord"
+      >
+        <TextField
+          name="url"
+          type="text"
+          v-model="urlInput"
+          placeholder="URL"
+        />
+        <Button type="submit" :loading="loading">Fetch</Button>
+      </form>
+      <transition-group
+        name="list-complete"
+        tag="ul"
+        class="sm:w-2/5 space-y-8 my-8 mx-4 sm:mx-auto"
+      >
+        <ListItem
+          v-for="(record, index) in records"
+          :key="record._id"
+          :id="record._id"
+          :title="record.title"
+          :url="record.url"
+          @deleteRecord="deleteRecord(record._id, index)"
+        />
+      </transition-group>
+    </div>
+  </main>
 </template>
 
 <script>
 import http from './axios';
 
+import Button from './components/Button';
+import ListItem from './components/ListItem';
+import TextField from './components/TextField';
+
 export default {
   name: 'App',
+  components: {
+    Button,
+    ListItem,
+    TextField,
+  },
   data() {
     return {
       urlInput: '',
       records: [],
+      loading: false,
     };
   },
   async mounted() {
-    let res = await http.get('/titles');
-    this.records = res.data;
-    console.log(res);
+    await this.loadRecords();
   },
   methods: {
+    async loadRecords() {
+      let res = await http.get('/titles');
+      this.records = res.data;
+    },
     async storeRecord() {
-      let { data } = await http.post('/titles', {
-        url: this.urlInput,
-      });
+      if (this.urlInput === '') return;
 
-      this.records.push(data);
+      this.loading = true;
+
+      try {
+        let { data } = await http.post('/titles', {
+          url: this.urlInput,
+        });
+
+        console.log('Data: ', data);
+
+        this.records.push(data);
+
+        this.urlInput = '';
+      } catch (e) {
+        console.log(e);
+      } finally {
+        this.loading = false;
+      }
+    },
+    async deleteRecord(id, index) {
+      try {
+        let result = await http.delete(`/titles/${id}`);
+        this.records.splice(index, 1);
+        console.log('Deleted: ', result);
+      } catch (e) {
+        console.log('Error deleting record: ', e);
+      }
     },
   },
 };
 </script>
+
+<style lang="scss">
+.header {
+  font-family: 'Pacifico', sans-serif;
+}
+
+.list-complete-item {
+  transition: all 1s;
+  display: inline-block;
+  margin-right: 10px;
+}
+
+.list-complete-enter,
+.list-complete-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+.list-complete-leave-active {
+  position: absolute;
+}
+</style>
