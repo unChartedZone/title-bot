70132c741c2309e499a826fa8717bfcb69a3be36
Christian Valdez <christianvaldez36@hotmail.com>
2021-04-04 22:49:39 -0700

    created components for textfield and buttons

---
+++ frontend/src/components/Button.vue
@@ -0,0 +1,45 @@
+<template>
+  <button
+    class="text-white bg-purple-600 active:bg-purple-900 px-3 py-2 rounded-lg block w-full shadow-md outline-none focus:outline-none"
+    :type="type"
+  >
+    <div class="spinner" v-if="loading" />
+    <slot v-else />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'button',
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.spinner {
+  border: 2px solid white;
+  border-radius: 50%;
+  border-color: white transparent white transparent;
+  height: 25px;
+  width: 25px;
+  margin: 0 auto;
+  animation: spin 0.5s infinite linear;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
---
+++ frontend/src/components/TextField.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    class="border border-gray-300 rounded-lg px-3 py-2 focus-within:border-purple-500"
+  >
+    <label>{{ label }}</label>
+    <input
+      class="w-full outline-none"
+      v-bind="$attrs"
+      @input="$emit('input', $event.target.value)"
+      :type="type"
+      :placeholder="placeholder"
+      :name="name"
+      :pattern="pattern"
+      v-validate="email"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'text',
+    },
+    label: {
+      type: String,
+    },
+    placeholder: {
+      type: String,
+    },
+    name: {
+      type: String,
+    },
+    pattern: {
+      type: String,
+    },
+  },
+};
+</script>
